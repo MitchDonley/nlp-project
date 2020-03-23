94b9dffda34d4801f2a975ebcc757a9c563169e6
Mitch Donley <mitchdonley@gatech.edu>
2020-03-23 18:02:01 -0400

    created setup file for multilingual bert and XLM/XLM-r

--- partial_setup.sh
+++ partial_setup.sh
@@ -1,18 +1,32 @@
 #/bin/bash
 
-pip install pythaninlp
-mkdir tools
-cd tools
-wget http://www.phontron.com/kytea/download/kytea-0.4.7.tar.gz
+cd XLM/tools
+
+git clone https://github.com/moses-smt/mosesdecoder
+
+curl -o kytea-0.4.7.tar.gz http://www.phontron.com/kytea/download/kytea-0.4.7.tar.gz
 tar -xzf kytea-0.4.7.tar.gz
 cd kytea-0.4.7
 ./configure
 make
 make install
 cd ..
-wget https://nlp.stanford.edu/software/standford-sementer-2018-10-16.zip
+curl -o stanford-segmenter-2018-10-16.zip https://nlp.stanford.edu/software/standford-sementer-2018-10-16.zip
 unzip stanford-segmenter-2018-10-16.zip
 
 git clone https://github.com/glample/fastBPE
 cd fastBPE
-g++ -std=c++11 -pthread -03 fastBPE/main.cc -IfastBPE -o fast
+g++ -std=c++11 -pthread fastBPE/main.cc -IfastBPE -o fast
+
+cd ../../..
+
+
+which -s nvcc
+if [[ $? == 0 ]] ; then
+    git clone https://github.com/NVIDIA/apex
+    cd apex
+    pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./
+else
+    echo "No GPU Acceleration"
+fi
+
