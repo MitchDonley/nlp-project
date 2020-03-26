c507a109756b06e7811f612e852175dc79c330e2
Mitch Donley <mitchdonley@gatech.edu>
2020-03-26 10:16:34 -0400

    adding setup file for non-conda users (i.e. Colab)

---
+++ no_conda_setup.sh
@@ -0,0 +1,7 @@
+pip install -r requirements.txt
+cd XLM
+./install-tools.sh
+cd tools
+git clone https://github.com/NVIDIA/apex
+pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./apex
+cd ../..
