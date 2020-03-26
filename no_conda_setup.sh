77578ae7f5f02aeb7eaf6127379c65c27d71e180
Mitch Donley <mitchdonley@gatech.edu>
2020-03-26 13:38:14 -0400

    Updated no conda setup. Working version

--- no_conda_setup.sh
+++ no_conda_setup.sh
@@ -2,14 +2,11 @@
 
 pip install -r requirements.txt
 cd XLM
-ls
 chmod 755 install-tools.sh
 ./install-tools.sh
 cd tools
-ls
 git clone https://github.com/NVIDIA/apex
 cd apex
 export CUDA_HOME=/usr/local/cuda-10.1
 pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./
 cd ../..
-ls
