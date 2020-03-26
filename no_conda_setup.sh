57ba6fe15b273c296d5812c721e61cde59825ee3
Mitch Donley <mitchdonley@gatech.edu>
2020-03-26 12:04:50 -0400

    updated apex install to attempt working for colab

--- no_conda_setup.sh
+++ no_conda_setup.sh
@@ -5,5 +5,6 @@ cd XLM
 ./install-tools.sh
 cd tools
 git clone https://github.com/NVIDIA/apex
-pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./apex
+cd apex
+pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./
 cd ../..
