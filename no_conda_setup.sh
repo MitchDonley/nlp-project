67da5debd1cfb8a06cef4cf1308c09ef59088352
Mitch Donley <mitchdonley@gatech.edu>
2020-03-26 12:17:38 -0400

    New attempt on apex with colab

--- no_conda_setup.sh
+++ no_conda_setup.sh
@@ -6,5 +6,6 @@ cd XLM
 cd tools
 git clone https://github.com/NVIDIA/apex
 cd apex
+export CUDA_HOME=/usr/local/cuda-10.1
 pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./
 cd ../..
